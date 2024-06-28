a619d9ed025eb58bd69b4a3e8663ef51dc4ea9f3
fatazakie <160807919+fatazakie@users.noreply.github.com>
2024-06-28 21:43:08 +0700

    buat controller barang

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    Route::get('barang', [HomeController::class, 'index']);
+    Route::get('barang', [BarangController::class, 'index']);
 
 });
